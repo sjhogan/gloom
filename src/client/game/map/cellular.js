84e36b4fb06b9435c2e7a9c47c314b4f21eefc35
Steve Hogan <sjhogan@loki.local>
2016-03-14 21:16:15 +1100

    Moved cellular map into separate file.

--- src/client/game/map/cellular.js
+++ src/client/game/map/cellular.js
@@ -1,3 +1,25 @@
-/**
- * Created by sjhogan on 14/03/2016.
- */
+import { Map as generator } from 'rot-js';
+import { getEmptyMap, Map } from './map';
+import { Tile }             from '../entity/tile';
+
+function getMapper(width, height, aliveRatio) {
+    const mapper = new generator.Cellular(width, height);
+
+    mapper.randomize(aliveRatio);
+
+    return mapper;
+}
+
+export function CellularMap(width, height, aliveRatio = 0.575, iterations = 5) {
+    const tiles     = getEmptyMap(width, height);
+    const mapper    = getMapper(width, height, aliveRatio);
+    const preseed   = iterations - 1;
+
+    for (let i = 0; i < preseed; i++) {
+        mapper.create();
+    }
+
+    mapper.create((x, y, alive) => tiles[x][y] = alive ? Tile.floorTile() : Tile.wallTile());
+
+    return Map(tiles);
+}
